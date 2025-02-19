0599a51e9a1996fdc8b6b0dec99daf62ba7c48c4
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-19 20:17:01 -0300

    req param complex

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,10 @@ app.get('/api/product/:id', (req: Request, res: Response):any => {
     return res.send(req.params);
 });
 
+app.get('/api/product/:id/review/:reviewId', (req: Request, res: Response):any => {
+    return res.send(req.params);
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
